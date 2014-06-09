fe832144121e3be13db262ddf15344fcc4969e27
Saurabh Joshi <battlex2010@yahoo.com>
2014-06-08 20:42:30 -0400

    Updated Facebook SDK

--- facebook/src/com/facebook/AppLinkData.java
+++ facebook/src/com/facebook/AppLinkData.java
@@ -21,13 +21,11 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
-
-import com.facebook.internal.AttributionIdentifiers;
-import com.facebook.internal.Utility;
-import com.facebook.internal.Validate;
+import com.facebook.internal.*;
 import com.facebook.model.GraphObject;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -368,7 +366,7 @@ public class AppLinkData {
     }
 
     /**
-     * This method has been deprecated. Please use {@link com.facebook.AppLinkData#getArgumentBundle()} instead.
+     * This method has been deprecated. Please use {@link AppLinkData#getArgumentBundle()} instead.
      * @return JSONObject property bag.
      */
     @Deprecated
